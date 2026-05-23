78eb79e61dac2b4e9a2c10970e7ea5e96e392cf8
Mamman Abdulhameed <mansjr@192.168.1.62>
2026-05-23 12:29:44 +0100

    Add Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {
 import { CartProvider } from "@/components/CartContext";
 
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -46,13 +47,43 @@ export default function RootLayout({
 
       <body>
 
-        <CartProvider>
+  {/* Meta Pixel */}
+  <Script
+    id="facebook-pixel"
+    strategy="afterInteractive"
+  >
+    {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
 
-          {children}
+      fbq('init', '1478223566860855');
+      fbq('track', 'PageView');
+    `}
+  </Script>
 
-        </CartProvider>
+  <noscript>
+    <img
+      height="1"
+      width="1"
+      style={{ display: "none" }}
+      src="https://www.facebook.com/tr?id=1478223566860855&ev=PageView&noscript=1"
+      alt=""
+    />
+  </noscript>
 
-      </body>
+  <CartProvider>
+
+    {children}
+
+  </CartProvider>
+
+</body>
 
     </html>
   );
